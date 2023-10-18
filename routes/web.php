5581a4be2f23c9bfcaaee5adb18103b7832f0cf7
J Marsh <jmarsh@Js-MacBook-Pro.attlocal.net>
2023-10-18 19:52:49 -0400

    feat: added dashboards per role type

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', DashboardController::class)->middleware(['auth'])->name('dashboard');
+Route::get('/admin/dashboard', function () { return view('admin.dashboard');})->middleware(['auth','role:admin'])->name('admin.dashboard');
+Route::get('/instructor/dashboard', function () { return view('instructor.dashboard');})->middleware(['auth','role:instructor'])->name('instructor.dashboard');
+Route::get('/member/dashboard', function () { return view('member.dashboard');})->middleware(['auth','role:member'])->name('member.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
