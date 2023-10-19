ec388c726677e4a8ea93d4d60ec5838deefa8bd0
J Marsh <jmarsh@Js-MacBook-Pro.attlocal.net>
2023-10-19 11:51:57 -0400

    Added User roles, gates, and policies, with the ability to create and delete upcoming classes

---
+++ app/Http/Controllers/ScheduledClassController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassType;
+use App\Models\ScheduledClass;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ScheduledClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $scheduledClasses = auth()->user()->scheduledClasses()->upcoming()->oldest('date_time')->get();
+
+        return view('instructor.upcoming')->with('scheduledClasses', $scheduledClasses);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $classTypes = ClassType::all();
+
+        return view('instructor.schedule')->with('classTypes', $classTypes);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $date_time = $request->input('date').' '.$request->input('time');
+        $request->merge([
+            'date_time' => $date_time,
+            'instructor_id' => auth()->user()->id,
+        ]);
+
+        $validator = $request->validate([
+            'class_type_id' => 'required',
+            'instructor_id' => 'required',
+            'date_time' => 'required|unique:scheduled_classes,date_time|after:now',
+        ]);
+
+        ScheduledClass::create($validator);
+
+        return redirect()->route('schedule.index');
+
+        // $validator = Validator::make($request->all(),[
+        //     'class_type_id' => 'required',
+        //     'instructor_id' => 'required',
+        //     'date_time' => 'required|unique:scheduled_classes,date_time|after:now'
+        // ]);
+
+        // if ($validated->fails()) {
+        //     //dd($request->all());
+        //     return redirect()->back()->withErrors($validator)->withInput($request->all());
+        // } else {
+        //     ScheduledClass::create($validator);
+        //     return redirect()->route('schedule.index');
+        // }
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ScheduledClass $schedule)
+    {
+        if (auth()->user()->cannot('delete', $schedule)) {
+            abort(403);
+        }
+        // if(auth()->user()->id !== $schedule->instructor_id) {
+        //     abort(403);
+        // }
+
+        $schedule->delete();
+
+        return redirect()->route('schedule.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ScheduledClassController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,12 +21,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', DashboardController::class)->middleware(['auth'])->name('dashboard');
-Route::get('/admin/dashboard', function () { return view('admin.dashboard');})->middleware(['auth','role:admin'])->name('admin.dashboard');
-Route::get('/instructor/dashboard', function () { return view('instructor.dashboard');})->middleware(['auth','role:instructor'])->name('instructor.dashboard');
-Route::get('/member/dashboard', function () { return view('member.dashboard');})->middleware(['auth','role:member'])->name('member.dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', DashboardController::class)->middleware(['auth'])->name('dashboard');
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->middleware(['role:admin'])->name('admin.dashboard');
+    Route::get('/instructor/dashboard', function () {
+        return view('instructor.dashboard');
+    })->middleware(['role:instructor'])->name('instructor.dashboard');
+    //Route::get('/member/dashboard', function () { return view('member.dashboard');})->middleware(['role:member'])->name('member.dashboard');
+
+    Route::resource('/instructor/schedule', ScheduledClassController::class)->only(['index', 'create', 'store', 'destroy'])->middleware(['role:instructor']);
+
+    /* Member routes */
+    Route::middleware(['role:member'])->group(function () {
+        Route::get('/member/dashboard', function () {
+            return view('member.dashboard');
+        })->name('member.dashboard');
+        Route::get('/member/book', [BookingController::class, 'create'])->name('booking.create');
+        Route::post('/member/bookings', [BookingController::class, 'store'])->name('booking.store');
+        Route::get('/member/bookings', [BookingController::class, 'index'])->name('booking.index');
+        Route::delete('/member/bookings/{id}', [BookingController::class, 'destroy'])->name('booking.destroy');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
